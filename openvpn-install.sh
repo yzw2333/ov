6c3d36bfe247965b42b66c26246bc34f4d62a694
weiwen zhong <azhongweiwen@sina.com>
2020-06-04 23:35:54 +0800

    Update openvpn-install.sh
    remove read from cmdline

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -83,7 +83,7 @@ if [[ -e /etc/openvpn/server.conf ]]; then
 else
 	# Autodetect IP address and pre-fill for the user
 	IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-	read -p "IP address: " -e -i $IP IP
+ 
 	# If $IP is a private IP address, the server must be behind NAT
 	if echo "$IP" | grep -qE '^(10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.|192\.168)'; then
 		echo
